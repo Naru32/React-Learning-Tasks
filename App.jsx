2311676d9519b7faa9ca81fbe2b5969d1358ad05
Narendra Mahalle <97868781+Naru32@users.noreply.github.com>
2024-12-23 11:34:10 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+// import GrandParent from "./GrandParent"
+// import Profile from "./Profile"
+import "./profile.css"
+import Parent from "./Parent"
+// import TaskCR from "./TaskCR"
+// import Task4 from "./Task4"
+//  import CounterContext from "../context/CounterContext"
+// import Registration from "./Registration"
+// import FetchData from "./FetchData"
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Parent/>)
